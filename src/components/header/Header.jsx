3a6572954f429f4b9b5aaba594854fec24e17266
Lovro Boric <lovro.boric@gmail.com>
2022-05-11 17:38:21 +0200

    header jsx and css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./header.css";
+import Me from "../../assets/l2.jpg";
+import Me2 from "../../assets/lovroBewerbungPhoto.png";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import { BsChevronDoubleDown } from "react-icons/bs";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="container header__container">
+      <h5>Hello,I'm</h5>
+      <h1>Lovro Boric</h1>
+      <h5 className="text-light">Fullstack developer</h5>
+      <CTA />
+
+      <a href="#contact" className="scroll__down">
+        Scroll Down
+        <BsChevronDoubleDown className="scrollIcon" />
+      </a>
+
+      <div className="me">
+        <img src={Me} alt="" srcset="" />
+      </div>
+      <HeaderSocials />
+    </div>
+  );
 };
 
 export default Header;
