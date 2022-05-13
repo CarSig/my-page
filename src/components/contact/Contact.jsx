3585f0eec91deda4536888a5068184aa8d6a7d32
Lovro Boric <lovro.boric@gmail.com>
2022-05-13 06:05:46 +0200

    sending email via emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 
 import { MdOutlineEmail } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    //TODO: add credentials
+
+    emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", form.current, "YOUR_PUBLIC_KEY").then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+    e.target.reset();
+    alert("message sent sucessfully");
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -30,7 +50,7 @@ const Contact = () => {
         </div>
 
         {/**END of contact options */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" rows="8" placeholder="Your Message" required></textarea>
