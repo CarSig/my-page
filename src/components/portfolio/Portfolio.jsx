b41a519f8de6fe6f7ddb61e4953095495dafdb0e
Lovro Boric <lovro.boric@gmail.com>
2022-05-25 05:40:02 +0200

    added links to project employee register

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./portfolio.css";
-import img1 from "../../assets/1.jpg";
+import img1 from "../../assets/2.jpg";
 
 import img4 from "../../assets/4.jpg";
 import img5 from "../../assets/5.jpg";
 
 const data = [
-  { id: 1, image: img1, github: "http://github.com", demo: "https://dribbble.com/ethnfndr", title: "finance app" },
-  { id: 2, image: img5, github: "http://github.com", demo: "https://dribbble.com/ethnfndr", title: "social media app" },
-  { id: 3, image: img4, github: "http://github.com", demo: "https://dribbble.com/ethnfndr", title: "airhelp app" },
+  {
+    id: 1,
+    image: img1,
+    github: "https://github.com/CarSig/employeeRegister",
+    demo: "https://mitarbeiter-register.herokuapp.com/",
+    title: "employee register app",
+  },
+  { id: 2, image: img5, github: "http://github.com", demo: "https://mitarbeiter-register.herokuapp.com/", title: "social media app" },
+  { id: 3, image: img4, github: "http://github.com", demo: "https://mitarbeiter-register.herokuapp.com/", title: "airhelp app" },
   // { id: 4, image: img3, github: "http://github.com", demo: "https://dribbble.com/ethnfndr", title: "tasktracker app" },
   // { id: 5, image: img4, github: "http://github.com", demo: "https://dribbble.com/ethnfndr", title: "fitness app" },
   // { id: 6, image: img5, github: "http://github.com", demo: "https://dribbble.com/ethnfndr", title: "another app" },
